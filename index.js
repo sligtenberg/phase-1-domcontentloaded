72f11970007c0817975476d49bb0dc30682227da
stephen ligtenberg <bountyhunter24@gmail.com>
2022-10-07 00:32:19 -0400

    commit tree 174f96bdefb0f61400ecbe6af3db9899343fbe5b

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    let textPar = document.getElementById("text");
+    textPar.textContent = 'This is really cool!';
+});
